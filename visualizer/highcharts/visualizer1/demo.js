e22d00b7cf84f01778c0e36e6c5fdde01b02aba6
Ben Gelsey <ben@bengelsey.com>
2016-01-26 12:23:13 -0500

    Setup HighCharts for visualizer

---
+++ visualizer/highcharts/visualizer1/demo.js
@@ -0,0 +1,80 @@
+function handleGo() {
+  var names_string = $('#names').val();
+  var names = names_string.split(',');
+  names = $.map(names, function(val) {
+    return val.trim();
+  });
+  createChart(names);
+};
+
+var createChart = function (input_names) {
+    var seriesOptions = [],
+        seriesCounter = 0,
+        names = input_names || ['MSFT', 'AAPL', 'GOOG'];
+
+    /**
+     * Create the chart when all data is loaded
+     * @returns {undefined}
+     */
+    function createChart() {
+
+        $('#container').highcharts('StockChart', {
+
+            rangeSelector: {
+                selected: 4
+            },
+
+            yAxis: {
+                labels: {
+                    formatter: function () {
+                        return (this.value > 0 ? ' + ' : '') + this.value + '%';
+                    }
+                },
+                plotLines: [{
+                    value: 0,
+                    width: 2,
+                    color: 'silver'
+                }]
+            },
+
+            plotOptions: {
+                series: {
+                    compare: 'percent'
+                }
+            },
+
+            tooltip: {
+                pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
+                valueDecimals: 2
+            },
+
+            series: seriesOptions
+        });
+    }
+
+    $.each(names, function (i, name) {
+        
+
+        $.getJSON('data/' + name + '.json', function (data) {
+
+            seriesOptions[i] = {
+                name: name,
+                data: data
+            };
+
+            // As we're loading the data asynchronously, we don't know what order it will arrive. So
+            // we keep a counter and create the chart when all the data is loaded.
+            seriesCounter += 1;
+
+            if (seriesCounter === names.length) {
+                createChart();
+            }
+        });
+    });
+}
+
+$(function(){
+  createChart();
+  $('#go').click(handleGo);
+});
+
